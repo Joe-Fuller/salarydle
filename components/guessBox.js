7403fff2d9ea124ef50f15b078e5b8e7ba9499a2
Joe <joefuller042@gmail.com>
2023-05-31 14:07:21 +0100

    update guess box css

--- components/guessBox.js
+++ components/guessBox.js
@@ -7,27 +7,29 @@ export default function GuessBox({ guesses, setGuesses, handleGuess, salary }) {
         className="grid justify-center bg-indigo-500 text-black "
         onSubmit={handleGuess}
       >
-        <label htmlFor="guess">Salary</label>
+        <label className="grid justify-center" htmlFor="guess">
+          Salary
+        </label>
         <input
           type="number"
           id="guess"
           name="guess"
-          maxlength="7"
+          maxLength="7"
           min="0"
           max="9999999"
           autoFocus
         ></input>
       </form>
-      <ul>
+      <div>
         {guesses.map((guess) => {
           return (
-            <li className="grid justify-center" key={guess}>
-              <p>{guess}</p>
-              <p>{guess < salary[0] ? "Too low" : "Too high"}</p>
-            </li>
+            <div key={guess} className="grid grid-cols-2">
+              <div>{guess}</div>
+              <div>{guess < salary[0] ? "Too low" : "Too high"}</div>
+            </div>
           );
         })}
-      </ul>
+      </div>
     </div>
   );
 }
