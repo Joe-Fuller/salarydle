d918e2639c3b6b1cf9cdff8818f2dd3d52246142
Joe <joefuller042@gmail.com>
2023-06-02 15:49:46 +0100

    add blur effect

--- components/jobAd.js
+++ components/jobAd.js
@@ -3,21 +3,21 @@ export default function JobAd({ job }) {
     <div className="bg-white text-black">
       <div className="text-xl font-bold">{job.title}</div>
       <div className="grid grid-cols-2">
-        <p>{job.location}</p>
-        <p>{job.company}</p>
+        <p className="blur-sm">{job.location}</p>
+        <p className="blur-sm">{job.company}</p>
       </div>
       <div>
         <p className="font-bold">Description</p>
-        <p>{job.description}</p>
+        <p className="blur-sm">{job.description}</p>
       </div>
       <div className="grid grid-cols-2">
         <div>
           <p className="font-bold">Requirements</p>
-          <p>{job.requirements}</p>
+          <p className="blur-sm">{job.requirements}</p>
         </div>
         <div>
           <p className="font-bold">Responsibilites</p>
-          <p>lalalalalalala responsbilites</p>
+          <p className="blur-sm">lalalalalalala responsbilites</p>
         </div>
       </div>
     </div>
