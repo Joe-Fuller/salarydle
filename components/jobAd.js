322b12065d8816f80e73ebb75d14ffdfc60bcbaf
Joe <joefuller042@gmail.com>
2023-06-02 15:52:36 +0100

    make text unblur as guesses are submitted

--- components/jobAd.js
+++ components/jobAd.js
@@ -3,21 +3,25 @@ export default function JobAd({ job, guesses }) {
     <div className="bg-white text-black">
       <div className="text-xl font-bold">{job.title}</div>
       <div className="grid grid-cols-2">
-        <p className="blur-sm">{job.location}</p>
-        <p className="blur-sm">{job.company}</p>
+        <p className={guesses.length < 1 ? "blur-sm" : ""}>{job.location}</p>
+        <p className={guesses.length < 2 ? "blur-sm" : ""}>{job.company}</p>
       </div>
       <div>
         <p className="font-bold">Description</p>
-        <p className="blur-sm">{job.description}</p>
+        <p className={guesses.length < 3 ? "blur-sm" : ""}>{job.description}</p>
       </div>
       <div className="grid grid-cols-2">
         <div>
           <p className="font-bold">Requirements</p>
-          <p className="blur-sm">{job.requirements}</p>
+          <p className={guesses.length < 4 ? "blur-sm" : ""}>
+            {job.requirements}
+          </p>
         </div>
         <div>
           <p className="font-bold">Responsibilites</p>
-          <p className="blur-sm">lalalalalalala responsbilites</p>
+          <p className={guesses.length < 5 ? "blur-sm" : ""}>
+            lalalalalalala responsbilites
+          </p>
         </div>
       </div>
     </div>
