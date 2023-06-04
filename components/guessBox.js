dd1198883e61a85926ea87795488bfb74ef05d53
Joe <joefuller042@gmail.com>
2023-06-04 10:33:19 +0100

    update guessBox css

--- components/guessBox.js
+++ components/guessBox.js
@@ -2,40 +2,39 @@
 
 export default function GuessBox({ guesses, setGuesses, handleGuess, salary }) {
   return (
-    <div className="grid justify-center">
-      <form
-        className="grid justify-center bg-indigo-500 text-black "
-        onSubmit={handleGuess}
-      >
-        <label className="grid justify-center" htmlFor="guess">
-          Salary
-        </label>
-        <input
-          type="number"
-          id="guess"
-          name="guess"
-          maxLength="7"
-          min="0"
-          max="9999999"
-          autoFocus
-        ></input>
-      </form>
-      <div>
-        {guesses.map((guess) => {
-          return (
-            <div key={guess} className="grid grid-cols-2">
-              <div>
-                {guess.toLocaleString("en-UK", {
-                  style: "currency",
-                  currency: "GBP",
-                })}
+    <div className="grid justify-center text-black">
+      <div className="bg-indigo-500 rounded-md p-1">
+        <form className="grid justify-center" onSubmit={handleGuess}>
+          <label className="grid justify-center" htmlFor="guess">
+            Salary
+          </label>
+          <input
+            type="number"
+            id="guess"
+            name="guess"
+            maxLength="7"
+            min="0"
+            max="9999999"
+            autoFocus
+          ></input>
+        </form>
+        <div>
+          {guesses.map((guess) => {
+            return (
+              <div key={guess} className="grid grid-cols-2">
+                <div>
+                  {guess.toLocaleString("en-UK", {
+                    style: "currency",
+                    currency: "GBP",
+                  })}
+                </div>
+                <div className="text-right">
+                  {guess < salary[0] ? "Too low" : "Too high"}
+                </div>
               </div>
-              <div className="text-right">
-                {guess < salary[0] ? "Too low" : "Too high"}
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
